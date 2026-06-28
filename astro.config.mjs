8d5094dcf69d193261d833704579b7d31d6acffa
drivin <philipp.lihs@gmail.com>
2026-06-28 21:31:55 +0200

    Pfade für Custom Domain korrigieren

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,7 @@
 import { defineConfig } from "astro/config";
 
-const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const repositoryOwner = process.env.GITHUB_REPOSITORY_OWNER;
-
-const pagesConfig =
-  isGitHubPages && repositoryName && repositoryOwner
-    ? {
-        site: `https://${repositoryOwner}.github.io`,
-        base: `/${repositoryName}`,
-      }
-    : {};
-
 export default defineConfig({
   output: "static",
-  ...pagesConfig,
+  site: "https://freiheitdurchpoesie.de",
+  base: "/",
 });
